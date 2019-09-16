a0b511151b951ef9f39af6ecd1d890be4c03a494
ricokritzer <rico@kritzer.net>
2019-09-16 16:22:43 +0200

    Grid hinzugefügt.

---
+++ Demo/src/de/ui/window/Grid.java
@@ -0,0 +1,32 @@
+package de.ui.window;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Grid extends JPanel // das ist eine Komponenete, auf die man Dinge platzieren kann
+{
+	public static void main(String[] args)
+	{
+		final EmptyWindow window = new EmptyWindow();
+		window.add(new Grid()); // wir nutzen das leere Fenster, fügen
+		window.setVisible(true); // wichtig, diese Zeile muss am Ende stehen, damit das Fenster neu gezeichnet
+									// wird
+	}
+
+	public Grid()
+	{
+		final int rows = 5;
+		final int cols = 5;
+		this.setLayout(new GridLayout(rows, cols)); // z. B. ein TabellenLayout.
+
+		this.add(new JButton("links oben"));
+		this.add(new JButton("rechts daneben"));
+		
+		for (int i = 0; i < 23; i++) //Schleife für die Erstellung weiterer Buttons
+		{
+			this.add(new JButton("weiterer Button"));
+		}
+	}
+}
