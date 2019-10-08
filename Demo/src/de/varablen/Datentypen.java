cd8713fdadfdd1b984cf441ac556b515d505d7bb
ricokritzer <rico@kritzer.net>
2019-10-08 20:21:05 +0200

    Datentypen - Arten

---
+++ Demo/src/de/varablen/Datentypen.java
@@ -0,0 +1,22 @@
+package de.varablen;
+
+public class Datentypen
+{
+	public Datentypen() // lediglich eine Auswahl an Datentypen
+	{
+		int ganzzahl = 0;
+		int kleinsteGanzzahl = Integer.MIN_VALUE;
+		int groessteGanzzahl = Integer.MAX_VALUE;
+
+		double fliesskommazahl = 1.2;
+		double kleinsteFliesskommazahl = Double.MIN_VALUE;
+		double groessteFliesskommazahl = Double.MAX_VALUE;
+
+		String text = "Hallo, ich bin ein Text";
+
+		boolean wahrheitswert = false;
+		boolean alternativerWahrheitswert = true;
+
+		char zeichen = 'z';
+	}
+}
